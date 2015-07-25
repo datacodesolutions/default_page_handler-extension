66330ba316025daaf16bd97b5e8e5092b481cbc1
Ryan Thompson <ryan@anomaly.is>
2015-07-25 17:49:28 -0500

    Update addon.php

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Default Page Handler',
+    'title'       => 'Default',
     'name'        => 'Default Page Handler Extension',
     'description' => 'The default page handler for the Pages module.'
 ];
