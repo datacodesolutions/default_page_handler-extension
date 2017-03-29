9a8b5cbff27711c125583f37f3e1e03d0e7a1729
Matt Davis <matt.davis@union.co>
2017-03-29 01:00:33 -0400

    remove unneeded content

--- src/Command/MakePage.php
+++ src/Command/MakePage.php
@@ -3,10 +3,8 @@
 use Anomaly\PagesModule\Page\Contract\PageInterface;
 use Anomaly\PagesModule\Page\PageAuthorizer;
 use Anomaly\PagesModule\Page\PageBreadcrumb;
-use Anomaly\PagesModule\Page\PageContent;
 use Anomaly\PagesModule\Page\PageLoader;
 use Anomaly\PagesModule\Page\PageResponse;
-
 
 /**
  * Class MakePage
@@ -39,14 +37,12 @@ class MakePage
      * Handle the command.
      *
      * @param PageLoader     $loader
-     * @param PageContent    $content
      * @param PageResponse   $response
      * @param PageAuthorizer $authorizer
      * @param PageBreadcrumb $breadcrumb
      */
     public function handle(
         PageLoader $loader,
-        PageContent $content,
         PageResponse $response,
         PageAuthorizer $authorizer,
         PageBreadcrumb $breadcrumb
@@ -55,7 +51,6 @@ class MakePage
         $breadcrumb->make($this->page);
         $loader->load($this->page);
 
-        $content->make($this->page);
         $response->make($this->page);
     }
 }
